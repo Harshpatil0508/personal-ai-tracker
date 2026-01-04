aab7c24a2c7bb4e495aeed429ee4ecab459ce4be
harshpatil0508 <patilharshvardhan9090@gmail.com>
2026-01-04 19:10:40 +0530

    feat: added role (user, admin) based login, added the refresh token

--- app/auth.py
+++ app/auth.py
@@ -1,27 +1,42 @@
-from datetime import datetime, timedelta,timezone
+from datetime import datetime, timedelta, timezone
 from jose import jwt
 from passlib.context import CryptContext
 from app.config import JWT_SECRET
-import hashlib
+from hashlib import sha256
 
 ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_HOURS = 1
+ACCESS_TOKEN_EXPIRE_MINUTES = 10
+REFRESH_TOKEN_EXPIRE_DAYS = 1
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+pwd_context = CryptContext(
+    schemes=["bcrypt"],
+    deprecated="auto"
+)
 
-def hash_password(password: str):
-    password_bytes = password.encode("utf-8")
-    safe_password = hashlib.sha256(password_bytes).digest()
-    return pwd_context.hash(safe_password)
+def hash_password(password: str) -> str:
+    return pwd_context.hash(password)
 
-def verify_password(password: str, hashed: str):
-    password_bytes = password.encode("utf-8")
-    safe_password = hashlib.sha256(password_bytes).digest()
-    return pwd_context.verify(safe_password, hashed)
+def verify_password(password: str, hashed: str) -> bool:
+    return pwd_context.verify(password, hashed)
 
-def create_access_token(user_id: int):
+def create_access_token(user_id: int, role: str) -> str:
     payload = {
-        "sub": str(user_id), # Must be string per JWT spec
-        "exp": datetime.now(timezone.utc) + timedelta(hours=ACCESS_TOKEN_EXPIRE_HOURS)
+        "sub": str(user_id),   # must be string
+        "role": role,
+        "type": "access",
+        "exp": datetime.now(timezone.utc)
+               + timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     }
     return jwt.encode(payload, JWT_SECRET, algorithm=ALGORITHM)
+
+def create_refresh_token(user_id: int):
+    expires_at = datetime.now(timezone.utc) + timedelta(days=REFRESH_TOKEN_EXPIRE_DAYS)
+    payload = {
+        "sub": str(user_id),
+        "type": "refresh",
+        "exp": expires_at
+    }
+    token = jwt.encode(payload, JWT_SECRET, algorithm=ALGORITHM)
+    token_hash = sha256(token.encode()).hexdigest()
+    return token, token_hash, expires_at
+
