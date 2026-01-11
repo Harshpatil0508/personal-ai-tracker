093a6f857a1ab9432a1a9426289e06c7a6648da9
abhipatil559 <87687111+abhipatil559@users.noreply.github.com>
2026-01-11 11:31:16 +0530

    feat: added get and delete apis for daily log

--- app/routers/logs.py
+++ app/routers/logs.py
@@ -6,7 +6,6 @@ from app.models import DailyLog
 from app.schemas import DailyLogCreate
 from app.dependencies import get_current_user_id
 from app.db import get_db
-
 router = APIRouter(prefix="/daily-logs", tags=["Daily Logs"])
 
 
@@ -43,3 +42,67 @@ def create_daily_log(
         "message": "Daily log saved successfully",
         "id": entry.id
     }
+    
+@router.get("/")
+def get_daily_log(
+    user_id: int = Depends(get_current_user_id),
+    db: Session = Depends(get_db)
+):
+    today = datetime.today().date()
+    log = db.query(DailyLog).filter(
+        DailyLog.user_id == user_id,
+        DailyLog.date == today
+    ).first()
+
+    if not log:
+        raise HTTPException(status_code=400, detail="No daily log found for today")
+
+    return log
+    
+@router.get("/all")
+def get_all_daily_log(
+    user_id: int = Depends(get_current_user_id),
+    db: Session = Depends(get_db)
+):
+    today = datetime.today().date()
+    log = db.query(DailyLog).filter(
+        DailyLog.user_id == user_id,
+    ).order_by(DailyLog.date.desc()).all()
+
+    if not log:
+        raise HTTPException(status_code=400, detail="No logs found for user")
+
+    return log
+    
+@router.delete("/")
+def delete_daily_log(
+    user_id: int = Depends(get_current_user_id),
+    db: Session = Depends(get_db)
+):
+    today = datetime.today().date()
+    log = db.query(DailyLog).filter(
+        DailyLog.user_id == user_id,
+    ).first()
+    
+    if not log:
+        raise HTTPException(status_code=400, detail="No log found for user for today")
+
+    db.delete(log)
+    db.commit()
+    
+    return {"message": "Today's daily log deleted"}
+
+@router.delete("/all")
+def delete_all_daily_log(
+    user_id: int = Depends(get_current_user_id),
+    db: Session = Depends(get_db)
+):
+
+    log = db.query(DailyLog).filter(
+        DailyLog.user_id == user_id,
+    ).delete()
+    
+    db.commit()
+    
+    return {"message": "User's all daily logs deleted"}
+    
